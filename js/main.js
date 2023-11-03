b7faaf72c4076d88f694b263ee0bef905e5fa3bf
Samuel Dugger <131627151+SamuelDugger@users.noreply.github.com>
2023-11-03 17:42:56 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls, show/hide the button
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  console.log("Scrolling");
+  console.log("window.scrollY:", window.scrollY);
+
+  if (window.scrollY > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
